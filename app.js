cec045b69cfdbf65afc763e615c89b903b7452ca
Milan5002 <milan.sana@kalvium.community>
2023-10-26 09:28:05 +0530

    done

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // Iteration 1: Making the play button in the index.html functional.
+let playbtn = document.getElementById("play-button")
+
+playbtn.onclick = () => {
+    // window.open("./game.html")
+    window.location.href = "./game.html"
+}
 // Description: When the play button is clicked the game.html page should be opened
+
--- score.js
+++ score.js
@@ -1,1 +1,16 @@
 // Iteration 8: Making scoreboard functional
+
+console.log(window.location.search)
+
+let params =new URLSearchParams(window.location.search);
+let score = params.get("score")
+
+let scoreboard=document.getElementById("score-board");
+scoreboard.textContent =score;
+
+let playagainbtn = document.getElementById("play-again-button")
+
+playagainbtn.onclick = () => {
+    // window.open("./game.html")
+    window.location.href = "./game.html"
+}
